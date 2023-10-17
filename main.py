61a4724343f1466b6c8118c9e876e51b23950c5e
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-17 07:28:29 +0000

    Modified my calculator

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-a = 20
-b = 30
-print("Printing the calculator")
-print("The addition of a and b is:", a + b)
-print("The subtraction of a and b is:", a - b)
-print("The multiplication of a and b is:", a * b)
-print("The division of a and b is:", a / b)
-print("The remainder of a and b is:", a % b)
-print("The exponent of a and b is:", a ** b)
-print("The floor division of a and b is:", a // b)
-print("The increment of a is:", a + 1)
-print("The decrement of a is:", a - 1)
-print("The increment of b is:", b + 1)
-print("The decrement of b is:", b - 1)
+print("Welcome to my calculator")
+a = input("Enter first number")
+b = input("Enter second number")
+
+print("The addition of", a ,"and", b ,"is:", int(a) + int(b))
+print("The subtraction of", a ,"and", b ,"is:", int(a) - int(b))
+print("The multiplication of", a ,"and", b ,"is:", int(a) * int(b))
+print("The division of", a ,"and", b ,"is:", int(a) / int(b))
+print("The remainder of", a ,"and", b ,"is:", int(a) % int(b))
